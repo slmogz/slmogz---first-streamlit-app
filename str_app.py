a9aa43d79fec168a7c2c20b425c0e73de721b323
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-22 11:44:58 +0300

    Update str_app.py

--- str_app.py
+++ str_app.py
@@ -1,8 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy dinner')
-
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
